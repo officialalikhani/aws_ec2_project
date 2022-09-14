c24013a7f70906d8a6963c63f284b8d87b023877
Alireza Alikhani <62111040+officialalikhani@users.noreply.github.com>
2022-09-14 12:14:20 +0430

    Update postgres.sh

--- db/postgres.sh
+++ db/postgres.sh
@@ -1,5 +1,19 @@
-FROM postgres
-ENV POSTGRES_PASSWORD P@ssw0rd
-ENV POSTGRES_DB postgres
-COPY postgres.sh /docker-entrypoint-initdb.d/
-RUN chmod +x /docker-entrypoint-initdb.d/postgres.sh
+#!/bin/sh
+psql << EOF
+-- Table: public.aws
+-- DROP TABLE public.aws;
+CREATE TABLE IF NOT EXISTS public.aws
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    id_vm  character varying COLLATE pg_catalog."default" NOT NULL,
+    ip_address character varying COLLATE pg_catalog."default" NOT NULL,
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    size_ character varying COLLATE pg_catalog."default" NOT NULL,
+    location_ character varying COLLATE pg_catalog."default" NOT NULL,
+    date character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT aws_pkey PRIMARY KEY (id)
+)
+TABLESPACE pg_default;
+ALTER TABLE public.aws
+    OWNER to postgres;
+EOF"
